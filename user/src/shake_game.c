f98f72f924a32346a47b929714d317495ade6de5
김정민 <59995736+jmk445@users.noreply.github.com>
2023-12-13 17:42:59 +0900

    feat : add shake_game

---
+++ user/src/shake_game.c
@@ -0,0 +1,91 @@
+#include "stm32f10x.h"
+
+#include "common.h"
+#include "shake_game.h"
+
+#define MEM_OFFSET(x, y) (volatile unsigned int *) (x + y);
+
+/* -----------     Configure     ----------- */
+unsigned int BASE_RCC = 0x40021000;
+unsigned int BASE_PORT_B = 0x40010C00;
+unsigned int OFFSET_APB2_CLOCK = 0x00000018;
+unsigned int OFFSET_PORT_CONF_LOW = 0x00;
+unsigned int OFFSET_PORT_CONF_HIGH = 0x04;
+unsigned int OFFSET_INPUT_DATA = 0x08;
+unsigned int OFFSET_BIT_SET_RESET = 0x10;
+
+
+void enable_ports() {
+  volatile unsigned int *REG_APB2_CLOCK = MEM_OFFSET(BASE_RCC, OFFSET_APB2_CLOCK);
+  *REG_APB2_CLOCK = 0x3C;
+}
+
+void set_input_output() {
+  volatile unsigned int *REG_PORT_B_LOW = MEM_OFFSET(BASE_PORT_B, OFFSET_PORT_CONF_LOW);   
+  *REG_PORT_B_LOW = 0x44844444;  
+}
+
+
+/* -----------    Game Control   ----------- */
+
+// sensor input : PB10
+int is_shaked(int btn) {  
+  volatile unsigned int *PORT_B_INPUT = MEM_OFFSET(BASE_PORT_B, OFFSET_INPUT_DATA);    
+  if (btn == 2) {
+    volatile unsigned int input = *PORT_B_INPUT;
+    return (input & 0x400) != 0x400;
+  }  
+  return 0;
+}
+
+void shakeGame_Configure(){
+    enable_ports();
+    set_input_output();
+}
+
+
+cur_player = 0;
+int count;
+
+
+void shakeGame_turnHandler(){
+    if(game_state == 0){  // 이제 턴 시작 (ready)
+    }
+    else{  // 해당 플레이어의 턴 종료
+        // 해당 플레이어의 값을 저장
+        values[cur_player] = count;
+        // 현재 플레이어의 값을 초기화
+        count = 0;
+        // 다음 플레이어로 넘어감
+        cur_player++;
+        // 모든 플레이어가 턴을 종료했는지 확인
+        if(cur_player == player_count)
+          allTurnEnd = 1;
+    }
+    // 게임 상태 변경
+    game_state = !game_state;
+}
+
+void shakeGame(){
+  // 모든 플레이어가 턴을 종료했을 때 게임 종료
+    if (allTurnEnd) {
+      cur_player = 0;
+      return 0;
+    }
+    // 게임 시작
+    int punch_pressure = 0;
+    game_state = 0;
+    while(!allTurnEnd){   // 플레이어의 턴일 때만 로직을 실행
+      while (game_state == 1){
+        if(is_shaked(2)){
+          count++;          
+        }   
+        if(count > 100){
+          count = 0;
+        }             
+      }
+   }
+}
+
+
+
